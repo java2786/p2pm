ce0808016b3e337bcaa2e44ad85b1e82d47f2013
Arun Kumar <java.kumar.arun@gmail.com>
2025-10-08 10:34:19 +0530

    8oct

---
+++ tutorial_4_problems/loop.py
@@ -0,0 +1,14 @@
+import time
+# print table -> 5
+
+num = 5
+count = 1
+
+# while(count<=10):
+#     print(f"{num} * {count} = {num*count}")
+#     count = count + 1
+    # time.sleep(1)
+
+for i in range(1,11):
+    print(f"{num} * {i} = {num*i}")
+
